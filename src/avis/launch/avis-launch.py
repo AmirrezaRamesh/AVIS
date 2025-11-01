9f5243426a88b2cca592a27aade27e349892bb39
shayan17shane19 <shayansetayandevolleyking5@gmail.com>
2025-11-01 20:54:35 +0330

    change atan2 to atan

--- src/avis/launch/avis-launch.py
+++ src/avis/launch/avis-launch.py
@@ -34,8 +34,8 @@ def generate_launch_description():
                 parameters=[{
                     'gain_yaw': 0.55,
                     'gain_crosstack_error': 0.2,
-                    'gain_yaw': 0.65,
-                    'gain_crosstack_error': 0.08,
+                    'gain_ks': 0.001,
+                    'steer_limit': 40,
                     'legal_error': 100.0
                 }]
             )]
