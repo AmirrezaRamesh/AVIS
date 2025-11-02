dc37acc7b7cfc90486cadd69abd2619519fb1f26
yuki261 <yegane.r261@gmail.com>
2025-11-02 15:58:05 +0330

    update

--- src/avis/launch/avis-launch.py
+++ src/avis/launch/avis-launch.py
@@ -33,9 +33,9 @@ def generate_launch_description():
                 respawn=True,
                 parameters=[{
                     'gain_yaw': 0.55,
-                    'gain_crosstack_error': 0.2,
+                    'gain_crosstack_error': 0.08,
                     'gain_ks': 0.001,
-                    'steer_limit': 40,
+                    'steer_limit': 40.0,
                     'legal_error': 100.0
                 }]
             )]
