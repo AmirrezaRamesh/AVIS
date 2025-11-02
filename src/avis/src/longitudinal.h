bf307564d6d09325805e70c3001ccebbe0c9052f
shayan17shane19 <shayansetayandevolleyking5@gmail.com>
2025-11-02 14:17:16 +0330

    update area to 10 section

--- src/avis/src/longitudinal.h
+++ src/avis/src/longitudinal.h
@@ -10,13 +10,13 @@ class Longitudinal
 private:
     PID speed_pid;
 
-    float curve_area[5];
+    float curve_area[10];
     float area_bound;
     float base_speed;
     float decreasment_speed;
 
 public:
-    Longitudinal(const float (&area)[5], float area_bound, float base_speed,float decreasment_speed);
+    Longitudinal(const float (&area)[10], float area_bound, float base_speed, float decreasment_speed, float (&pid_gains)[5]);
     int determine_area(float carvature);
     float get_speed(float curvature);
     float set_speed(float curvature, float current_speed);
